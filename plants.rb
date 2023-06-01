37085fe34d1beca9381f97034eda2ea90c57bd21
Jorge Zambrana <zee7man@yahoo.com>
2023-06-01 15:22:18 -0600

    Add or statement

--- plants.rb
+++ plants.rb
@@ -1,7 +1,9 @@
 gets_sunshine = false
 ounces_of_water = 10
 if gets_sunshine && ounces_of_water >= 10
-    puts "The plant will thrive!"
+    puts "The plant will take over"
+elsif gets_sunshine || ounces_of_water >= 10
+    puts "It has got a shot"
 else 
     puts "The plant will die!"
 end
